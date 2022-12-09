2f5f045082c192d4c5106d8013004405ae51a9fd
Harpica <harpy.ka@gmail.com>
2022-12-09 17:39:02 +0300

    feat: lesson #8

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 
 // express app
 const app = express();
@@ -9,6 +10,20 @@ app.set('views', 'views'); // where to find views (folder)
 
 // listen for requiests
 app.listen(3000); // local host
+
+//middleware & static files (public, access by browser)
+app.use(express.static('public')); // everything in puplic folder is avaliable in frontend
+
+// app.use((req, res, next: Function) => {
+//   console.log('new request made:');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   // allows browser to move to the next line of code
+//   next();
+// });
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   //   res.send(); // automatically setHead, status code
